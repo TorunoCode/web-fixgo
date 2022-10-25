222acb1f3958e0671f8eaa1712e0c92c076bbcbd
Alicutun <xxtrantienphatxx@gmail.com>
2022-10-25 10:15:05 +0700

    fix deploy

--- src/components/ListMovies.jsx
+++ src/components/ListMovies.jsx
@@ -17,43 +17,45 @@ const ListMovie = ({ list }) => {
     setQuantityShow(5);
     setTotalQuantity(listMovie?.length - 5);
   }, [listMovie]);
+
   const handleShowViewMore = (e) => {
     setQuantityShow((prev) => prev + 5);
     setTotalQuantity((prev) => prev - 5);
   };
-
   return (
     <div>
       <div className="listMovie">
-        {listMovie?.slice(0, quantityShow).map((item) => (
-          <Link
-            to={`/MovieDetail/${item.name}`}
-            style={{ textDecoration: "none" }}
-          >
-            <div className="itemMovie" key={item?._id}>
-              <StarRating rating={item.rate} />
-              <img src={item.image} alt="" />
-              {item.name.length > 17 ? (
-                <Marquee
-                  pauseOnHover={true}
-                  speed={100}
-                  loop={1}
-                  delay={5}
-                  className="name"
-                >
-                  {item.name.toLowerCase()}
-                </Marquee>
-              ) : (
-                <div className="name">{item.name.toLowerCase()}</div>
-              )}
-              <div className="genre-rate">
-                Genre: {item.genre.toLowerCase()} <br />
-                Rate: {item.rate}/10
-              </div>
-              <div className="btn_buy">BUY TICKET</div>
-            </div>
-          </Link>
-        ))}
+        {Array.isArray(listMovie)
+          ? listMovie?.slice(0, quantityShow).map((item, index) => (
+              <Link
+                to={`/MovieDetail/${item.name}`}
+                style={{ textDecoration: "none" }}
+              >
+                <div className="itemMovie" key={index}>
+                  <StarRating rating={item.rate} />
+                  <img src={item.image} alt="" />
+                  {item.name.length > 17 ? (
+                    <Marquee
+                      pauseOnHover={true}
+                      speed={100}
+                      loop={1}
+                      delay={5}
+                      className="name"
+                    >
+                      {item.name.toLowerCase()}
+                    </Marquee>
+                  ) : (
+                    <div className="name">{item.name.toLowerCase()}</div>
+                  )}
+                  <div className="genre-rate">
+                    Genre: {item.genre.toLowerCase()} <br />
+                    Rate: {item.rate}/10
+                  </div>
+                  <div className="btn_buy">BUY TICKET</div>
+                </div>
+              </Link>
+            ))
+          : null}
       </div>
       <div className="button_xemthem">
         {totalQuantity <= 0 ? (
